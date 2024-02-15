7245c427f0f333105e3b5e90f67deeabd21da852
Vladislav <vlad.chertorygin18@gmail.com>
2024-02-15 13:35:19 +0200

    config microservice architecture

---
+++ geography-service/src/main/java/com/example/geographyservice/GeographyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.geographyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeographyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeographyServiceApplication.class, args);
+    }
+
+}
---
+++ marketplace-service/src/main/java/com/example/marketplaceservice/MarketplaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketplaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceServiceApplication.class, args);
+    }
+
+}
