a2c622f256bdc555494d479a1bffb83f652044cf
liubcheck <lyubomirvs@gmail.com>
2024-02-18 22:41:00 +0100

    Transition from .properties to .yaml files in the apiGateway service module

--- transaction-service/src/main/java/com/example/transactionservice/controller/PixelTransactionController.java
+++ transaction-service/src/main/java/com/example/transactionservice/controller/PixelTransactionController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/pixe-transactions")
+@RequestMapping("/api/pixel-transactions")
 @RequiredArgsConstructor
 public class PixelTransactionController {
     private final PixelTransactionService pixelTransactionService;
