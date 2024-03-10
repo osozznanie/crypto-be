7b405a6b4dd607b12d2fee12b39e4d45ece12a15
Vladislav <vlad.chertorygin18@gmail.com>
2024-03-11 00:49:44 +0200

    World statistics

---
+++ analytic-service/src/main/java/com/example/analyticsservice/worldStatistics/service/impl/WorldStatisticsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.analyticsservice.worldStatistics.service.impl;
+
+import com.example.analyticsservice.feign.WorldFeign;
+import com.example.analyticsservice.worldStatistics.dto.WorldStatistics;
+import com.example.analyticsservice.worldStatistics.service.WorldStatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WorldStatisticsServiceImpl implements WorldStatisticsService {
+    private final WorldFeign worldFeign;
+
+    @Override
+    public Long getTotalPixels() {
+        return worldFeign.getTotalPixels();
+    }
+
+    @Override
+    public Long getTotalSoldPixels() {
+        return worldFeign.getTotalSoldPixels();
+    }
+
+    @Override
+    public WorldStatistics getWorldStatistics() {
+        WorldStatistics worldStatistics = WorldStatistics.builder()
+                .totalPixels(getTotalPixels())
+                .soldPixels(getTotalSoldPixels())
+                .build();
+
+        worldStatistics.setRemainingPixels(worldStatistics.remainingPixels());
+        worldStatistics.setPercentageSold(worldStatistics.getPercentageSold());
+
+        return worldStatistics;
+    }
+}
