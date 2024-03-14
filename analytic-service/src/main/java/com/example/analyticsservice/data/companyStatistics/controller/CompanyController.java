b127ec8a23e5b638d907efff5cdcea508c643424
Vladislav <vlad.chertorygin18@gmail.com>
2024-03-14 09:19:29 +0200

    some questions!!!

--- analytic-service/src/main/java/com/example/analyticsservice/data/companyStatistics/controller/CompanyController.java
+++ analytic-service/src/main/java/com/example/analyticsservice/data/companyStatistics/controller/CompanyController.java
@@ -1,7 +1,7 @@
 package com.example.analyticsservice.data.companyStatistics.controller;
 
 import com.example.analyticsservice.data.companyStatistics.dto.response.CompanyResponseDto;
-import com.example.analyticsservice.data.companyStatistics.serivce.CompanyService;
+import com.example.analyticsservice.data.companyStatistics.serivce.CompanyStatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,25 +13,30 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/companies-statistics")
 public class CompanyController {
-    private final CompanyService companyService;
+    private final CompanyStatisticsService companyStatisticsService;
 
     @GetMapping("/amount")
     public Long getAmountOfCompanies() {
-        return companyService.getAmountOfCompanies();
+        return companyStatisticsService.getAmountOfCompanies();
     }
 
     @GetMapping("/top20")
     public List<CompanyResponseDto> getTop20Companies() {
-        return companyService.getTopCompanies(20);
+        return companyStatisticsService.getTopCompanies(20);
     }
 
     @GetMapping("/top3")
     public List<CompanyResponseDto> getTop3Companies() {
-        return companyService.getTopCompanies(3);
+        return companyStatisticsService.getTopCompanies(3);
     }
 
 //    @GetMapping("/{tag}")
 //    public CompanyResponseDto getCompanyStats(@PathVariable String tag) {
-//        return companyService.getCompanyStats(tag);
+//        return companyStatisticsService.getCompanyStats(tag);
 //    }
+
+    @GetMapping("/top-companies-by-continent")
+    public List<CompanyResponseDto> getTopCompaniesByContinent() {
+        return companyStatisticsService.getTopCompaniesByContinent();
+    }
 }
